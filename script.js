c371a4c0e20ae7b3b6e0526b4c64762792114177
JamieHallett <jamie66.hallett@gmail.com>
2025-01-27 19:44:00 +0000

    Add webgl demo stuff

--- script.js
+++ script.js
@@ -1,36 +1,168 @@
+import { initBuffers } from "./init-buffers.js";
+import { drawScene } from "./draw-scene.js";
+
+const workers = {
+  a: undefined,
+};
+
 function button() {
   fetch("text.txt")
-  .then(function (res) {
-    return res.text();
-  })
-  .then(function (data) {
-    //console.log(data);
-    alert(data);
-  });
-
+    .then(function (res) {
+      return res.text();
+    })
+    .then(function (data) {
+      //console.log(data);
+      alert(data);
+    });
 }
 
-/* this works no problem on node.js: 
-https://replit.com/@jamiehallett1/ViolentUnwillingAlgorithm#index.js
-but does it work in html,css,js? */
 function button2() {
-  const { spawn } = require('child_process');
-
-  const pythonProcess = spawn('python', ['doesthiswork.py']);
-
-  pythonProcess.stdout.on('data', (data) => {
-    console.log(`Python script output: ${data}`);
-  });
-
-  pythonProcess.stderr.on('data', (data) => {
-    console.error(`Error in Python script: ${data}`);
-  });
-
-  pythonProcess.on('close', (code) => {
-    console.log(`Python script process exited with code ${code}`);
-  });
+  makeWorker(
+    function () {
+      for (let i = 4; i > 0; i--) {
+        postMessage(`press ok ${i} more times`);
+      }
+    },
+    function (e) {
+      alert(e.data);
+    },
+    "a",
+  );
 }
 
-button2()
+function makeWorker(workerfunc, receivefunc, workername) {
+  // Build a worker from an anonymous function body
+  let blobURL = URL.createObjectURL(
+    // Create a temporary url to the blob of the worker
+    new Blob(["(", workerfunc.toString(), ")()"], {
+      // When the blob's code is run, it will call workerfunc
+      type: "application/javascript",
+    }),
+  );
+  workers[workername] = new Worker(blobURL); // Use the url to create the worker
+  workers[workername].onmessage = receivefunc; // Set receivefunc to be called when the worker sends a message
+  // Won't be needing this anymore
+  URL.revokeObjectURL(blobURL);
+}
 
-//the answer: no
+/* more stuff 
+https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context
+*/
+const canvas = document.querySelector("#canvas");
+// Initialize the GL context
+const gl = canvas.getContext("webgl");
+
+// Set clear color to black, fully opaque
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+// Clear the color buffer with specified clear color
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+// Vertex shader program
+
+const vsSource = `
+    attribute vec4 aVertexPosition;
+    attribute vec4 aVertexColor;
+
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    varying lowp vec4 vColor;
+
+    void main(void) {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      vColor = aVertexColor;
+    }
+  `;
+
+// Fragment shader program
+
+const fsSource = `
+    varying lowp vec4 vColor;
+
+    void main(void) {
+      gl_FragColor = vColor;
+    }
+  `;
+
+//
+// Initialize a shader program, so WebGL knows how to draw our data
+//
+function initShaderProgram(gl, vsSource, fsSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  // Create the shader program
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  // If creating the shader program failed, alert
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert(
+      `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+        shaderProgram,
+      )}`,
+    );
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+//
+// creates a shader of the given type, uploads the source and
+// compiles it.
+//
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  // Send the source to the shader object
+
+  gl.shaderSource(shader, source);
+
+  // Compile the shader program
+
+  gl.compileShader(shader);
+
+  // See if it compiled successfully
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(
+      `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`,
+    );
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+// Initialize a shader program; this is where all the lighting
+// for the vertices and so forth is established.
+const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+// Collect all the info needed to use the shader program.
+// Look up which attributes our shader program is using
+// for aVertexPosition, aVertexColor and also
+// look up uniform locations.
+const programInfo = {
+  program: shaderProgram,
+  attribLocations: {
+    vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+    vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
+  },
+  uniformLocations: {
+    projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
+    modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+  },
+};
+
+// Here's where we call the routine that builds all the
+// objects we'll be drawing.
+const buffers = initBuffers(gl);
+
+// Draw the scene
+drawScene(gl, programInfo, buffers);
